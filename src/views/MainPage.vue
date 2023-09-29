6aa7539b31b78082550e869e53efeec421eecee1
Techini331 <som168a@gmail.com>
2023-09-29 23:05:55 +0700

    first commit

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -34,12 +34,12 @@ const showBusinessDetails = (businessId) => {
 onMounted(async () => {
  
   businesses.value = [
-    { name: 'Smoothcup  Coffee', imageSrc: 'src/assets/Smoothcup Coffee.jpg' },
-    { name: 'Chill  Coffee', imageSrc: 'src/assets/Chill Coffee.jpg' },
-    { name: 'Yensabai  Coffee', imageSrc: 'src/assets/Yensabai Coffee.jpg' },
-    { name: 'Lamoon Coffee', imageSrc: 'src/assets/Lamoon Coffee.jpg' },
-    { name: 'Lucky  Coffee', imageSrc: 'src/assets/Lucky Coffee.jpg' },
-    { name: 'Goodmood  Coffee', imageSrc: 'src/assets/Goodmood Coffee.jpg' },
+    { name: 'Smoothcup  Coffee', imageSrc: 'https://image.makewebeasy.net/makeweb/m_1920x0/sEBH5Q7g0/SHOP2/WebHug2_1.jpg' },
+    { name: 'Chill  Coffee', imageSrc: 'https://i.pinimg.com/564x/77/0a/1e/770a1e29215dc0a3398c3c8cf882efb1.jpg' },
+    { name: 'Yensabai  Coffee', imageSrc: 'https://i.pinimg.com/564x/22/f5/1e/22f51e1e04194c4ea668b9ec05241452.jpg' },
+    { name: 'Lamoon Coffee', imageSrc: 'https://i.pinimg.com/564x/58/e9/e7/58e9e710cff1fbd332ff6c85c2df4f7d.jpg' },
+    { name: 'Lucky  Coffee', imageSrc: 'https://i.pinimg.com/564x/6e/c7/c3/6ec7c3c92d2b1fce462cfbdb9ceaab92.jpg' },
+    { name: 'Goodmood  Coffee', imageSrc: 'https://i.pinimg.com/564x/a5/33/81/a533814d24c7c9a5c6727429b8780eee.jpg' },
   ];
   
 });
